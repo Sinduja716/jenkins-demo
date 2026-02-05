bb008fa110c19a97fcef148c9e4c640594568782
Sinduja716 <sinduja.k2023@vitstudent.ac.in>
2026-02-05 12:35:17 +0530

    new change

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Mini CI Project Successful");
+        System.out.println("Mini Change done");
     }
 }
